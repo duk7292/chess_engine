48fc96f858364add890e40ba4ac00acc250a11e0
duk7292 <duk7292@gmail.com>
2024-04-28 17:19:16 +0200

    c

---
+++ build/CMakeFiles/ChessEngine.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/duk/Documents/C++/chess_engine/Chess_Library/Bitboards.cpp" "CMakeFiles/ChessEngine.dir/Chess_Library/Bitboards.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/Chess_Library/Bitboards.cpp.o.d"
+  "/home/duk/Documents/C++/chess_engine/Chess_Library/MinMax.cpp" "CMakeFiles/ChessEngine.dir/Chess_Library/MinMax.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/Chess_Library/MinMax.cpp.o.d"
+  "/home/duk/Documents/C++/chess_engine/Main.cpp" "CMakeFiles/ChessEngine.dir/Main.cpp.o" "gcc" "CMakeFiles/ChessEngine.dir/Main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ChessEngine.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessEngine.dir/Chess_Library/Bitboards.cpp.o"
+  "CMakeFiles/ChessEngine.dir/Chess_Library/Bitboards.cpp.o.d"
+  "CMakeFiles/ChessEngine.dir/Chess_Library/MinMax.cpp.o"
+  "CMakeFiles/ChessEngine.dir/Chess_Library/MinMax.cpp.o.d"
+  "CMakeFiles/ChessEngine.dir/Main.cpp.o"
+  "CMakeFiles/ChessEngine.dir/Main.cpp.o.d"
+  "ChessEngine"
+  "ChessEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
